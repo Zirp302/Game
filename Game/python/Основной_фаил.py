73973024d780695e1a238d146fbc6c954dd03f06
Zirp302 <vkkakantakt@gmail.com>
2025-08-11 14:24:00 +0500

    Add files via upload

--- Game/python/Основной_фаил.py
+++ Game/python/Основной_фаил.py
@@ -8,13 +8,8 @@ pygame.display.set_caption('Мёртвая точка')
 
 x_save = 0
 y_save = 0
-def pl(x=0, y=0, x_save=300, y_save=300, rgb=(0,0,0), w=20, h=50):
-    Hit_box = pygame.Rect(x, y, 0 + w, 0 + h)
-    pygame.draw.rect(screen, (0, 0, 0), Hit_box)
-    return x, y
 
-
-def Upravlenie(x_save, y_save, speed=5):
+def player(x_save, y_save, w=20, h=50, speed=5):
     if k.is_pressed('w'):
         x_save += 0
         y_save += -speed
@@ -28,18 +23,31 @@ def Upravlenie(x_save, y_save, speed=5):
         x_save += speed
         y_save += 0
 
-    pl(x_save, y_save)
+    Hit_box = pygame.Rect(x_save, y_save, w, h)
+    pygame.draw.rect(screen, (0, 0, 0), Hit_box)
     return (x_save, y_save)
 
 
+def stena(x=0, y=0, w=100, h=75):
+    stena = pygame.Rect(x, y, w, h)
+    pygame.draw.rect(screen, (115,66,34), stena)
+    
+
+def Dom(x=0,y=0,w_dom=1.25,h_dom=1.25):
+    w = w_dom * 100
+    h = h_dom * 75
+    obsh_mnog = 100
+    stena(x - w, y - h, w * 2.75, h)      # Левый верхний угол    # Верхняя стена
+    stena(x - w, y + h, w * 2.75, h)      # Левый нижний угол     # Нижняя стена
+    stena(x + w, y - h, h, h * 2.75) # Правый верхний угол   # Правая стена
 
 
 
 Run = True
 while Run:
     screen.fill((63, 155, 11))   # Цвет заднего фона 
-    x_save, y_save = Upravlenie(x_save,y_save)
-
+    x_save, y_save = player(x_save, y_save)
+    Dom(360,360)
     
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
