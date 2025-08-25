bcaa46159c0b37ec6e59fae1634c8d96025d21c1
Zirp302 <vkkakantakt@gmail.com>
2025-08-25 13:30:31 +0500

    к

--- Game/python/Objekts.py
+++ Game/python/Objekts.py
@@ -1,30 +1,28 @@
-import pyglet
-import time
 from pyglet import shapes as sh
-from pyglet.window.key import *
 from Uprav import Uprav
 
+
 class Pl:
+    # Создание самого игрока
+    playr = None
     def __init__(self, x, y, width, height, color, batch):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
-        self.batch = batch 
-        self.playr = sh.Rectangle(x, y, width, height, color, batch=batch)
+        self.batch = batch
+        Pl.playr = sh.Rectangle(x, y, width, height, color, batch=batch)
+        
 
-    
-    def pl_moving(self, x_speed, y_speed, speed=5):
+    # HP игрока
+    def HP(self):
+        playr = Pl.playr
         HP_playr = 5
         HP_One = self.width / HP_playr
-        HP = sh.Rectangle(self.playr.x, self.playr.y + self.height, HP_playr * HP_One, 15, color=(255,0,0), batch=self.batch)
-        key = Uprav(x_speed, y_speed, self.playr, HP, self.width)
-
-
+        Polosa = HP_playr * HP_One # Полоска HP
+        return sh.Rectangle(playr.x, playr.y + self.height, Polosa, 15, color=(255,0,0), batch=self.batch)
 
     def draw(self):
-        self.playr.draw()
-    
-
+        Pl.playr.draw()
 
