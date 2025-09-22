3558819fc6b6f91943e68a8460c442cc2072cb22
beward-it <anotheronemailkotopom@gmail.com>
2025-09-22 06:14:38 +0500

    тут будет моя песочница

--- Game/python/B.py
+++ Game/python/B.py
@@ -16,7 +16,7 @@ wind = pyglet.window.Window(width=wind_width, height=wind_height, caption="gameO
 width, height = (50, 100)
 playr = Pl()
 xp=100
-pist = Ognestrel(playr)
+pist = Ognestrel(playr, mugSpeed=30, kd=0.05, mugsNow=100000, MaxMugsNum=1000, damag=100)
 Uprav = playrUprav(playr, playr.HP, pist)
 #могут ли зомби появляться
 #Скорость появления зомбей попробуй изменить число на какое нибудь оч маленькое по типу 1/60 и тд
@@ -27,8 +27,8 @@ drawInfuncs=[]
 @wind.event
 def on_mouse_press(x,y,button,modifiers):
     print(f"x = {x}, y = {y}")
-defaultZomb = Zombi(playr, Uprav)
-MiniBoss = Zombi(playr=playr,plrUprv=Uprav, width=25, height=25, type="big", xp=1000, speed=0.2, spawnSpeed=3, damage=30)
+defaultZomb = Zombi(playr, Uprav, spawnSpeed=1/30)
+MiniBoss = Zombi(playr=playr, plrUprv=Uprav, width=70, height=70, type="big", xp=10000, speed=0.2, spawnSpeed=1, damage=50, col=(21, 110, 100))
 
 
 
@@ -61,7 +61,7 @@ def Damag(dt, HP_One=HP.HP_One):
 
 keys = key.KeyStateHandler()
 wind.push_handlers(keys)
-walls = Stena()
+walls = Stena(playr, Uprav)
 
 def avanpost(xM, yM):
     stena_l = Uprav.ogran_line(walls.left_S[0], walls.left_S[1], walls.left_S[2], walls.left_S[3], xM, yM)
@@ -98,7 +98,7 @@ defaultZomb.test(360, 360, 30, 30)
 @wind.event
 def on_draw():
     wind.clear()
-    Stena().draw()
+    walls.draw()
     playr.draw()
     #кстати чтобы определить цвет я использую https://colorscheme.ru/color-names.html
     defaultZomb.batch.draw()
@@ -111,11 +111,12 @@ while True:
     os.mkdir("hihi" + str(i))
     i += 1"""
 #defaultZomb.spawn()
-#pyglet.clock.schedule_interval(defaultZomb.spawn,defaultZomb.spawSpeed)
-#pyglet.clock.schedule_interval(defaultZomb.moving,1/20)
-pyglet.clock.schedule_interval(pist.pulaMoving, 1/3000)
+#pyglet.clock.schedule_interval(defaultZomb.spawn,defaultZomb.spawnSpeed)
+pyglet.clock.schedule_interval(defaultZomb.moving,1/15)
+pyglet.clock.schedule_interval(pist.pulaMoving, 1/100)
 #передвижения зомбей с обновлением каждые 1/4 секунды может уже не 1/4 
 pyglet.clock.schedule_interval(update, 1/60)
-#pyglet.clock.schedule_interval(defaultZomb.attack, 1)
+pyglet.clock.schedule_interval(defaultZomb.attack, 1)
 pyglet.clock.schedule_interval(pist.damage, 1/150)
+pyglet.clock.schedule_interval(MiniBoss.spawn, MiniBoss.spawnSpeed)
 pyglet.app.run()
