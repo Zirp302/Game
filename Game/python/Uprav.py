881362a60060baad41b972d9d774852c6c7a28b8
Zirp302 <vkkakantakt@gmail.com>
2025-10-03 22:37:09 +0500

    Update Uprav.py
    Исправление по за дауном

--- Game/python/Uprav.py
+++ Game/python/Uprav.py
@@ -13,26 +13,20 @@ class Playr_uprav:
         self.damag = Damag(playr)
         self.stena = Stena(playr.width)
     #   Передвижеие игрока и его полоски жизни
-    def pl_moving(self, keys):
-        x_mov = 0
-        y_mov = 0
-        #это было нужно чтобы игрок мог ходить по диагонали
-        #Если у персонажа х уже поменялся то он не должен ставиться обратно на ноль он должен оставатся такой как есть 
+    def pl_moving(self, keys): 
         if keys[W]:
-            x_mov, y_mov = x_mov, self.speed
+            x_mov, y_mov = 0, self.speed
         if keys[S]:
-            x_mov, y_mov = x_mov, -self.speed
+            x_mov, y_mov = 0, -self.speed
         if keys[A]:
-            x_mov, y_mov = -self.speed, y_mov
+            x_mov, y_mov = -self.speed, 0
         if keys[D]:
-            x_mov, y_mov = self.speed, y_mov
+            x_mov, y_mov = self.speed, 0
         avanpost = Stena.avanpost(self.playr, x_mov, y_mov)
         if 0 < x_mov + self.playr.x < 721 - self.playr.width and avanpost:
             self.playr.x += x_mov
-            self.playr.playr.x = self.playr.x
             self.HP.x += x_mov
         if 0 < y_mov + self.playr.y < 721 - self.playr.width and avanpost:
             self.playr.y += y_mov
-            self.playr.playr.y = self.playr.y
             self.HP.y += y_mov
             
