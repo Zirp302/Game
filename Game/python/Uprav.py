16b5971ca82357a4a0a2f4122fe55cab348f6b2b
Zirp302 <vkkakantakt@gmail.com>
2025-10-03 23:08:39 +0500

    Update Uprav.py

--- Game/python/Uprav.py
+++ Game/python/Uprav.py
@@ -1,30 +1,22 @@
 import pyglet
 from pyglet import shapes as sh
 from pyglet.window.key import *
+from Objeсts import Pl, Stena
 
 #   Управление 
-class Playr_uprav:
-    def __init__(self, playr,speed=5):
+class Uprav:
+    def __init__(self, playr, HP, width):
         self.playr = playr
-        self.HP = playr.HP
-        self.speed = speed
-        self.damag = Damag(playr)
-        self.stena = Stena(playr.width)
-    #   Передвижеие игрока и его полоски жизни
-    def pl_moving(self, keys): 
-        if keys[W]:
-            x_mov, y_mov = 0, self.speed
-        if keys[S]:
-            x_mov, y_mov = 0, -self.speed
-        if keys[A]:
-            x_mov, y_mov = -self.speed, 0
-        if keys[D]:
-            x_mov, y_mov = self.speed, 0
-        avanpost = Stena.avanpost(self.playr, x_mov, y_mov)
-        if 0 < x_mov + self.playr.x < 721 - self.playr.width and avanpost:
-            self.playr.x += x_mov
-            self.HP.x += x_mov
-        if 0 < y_mov + self.playr.y < 721 - self.playr.width and avanpost:
-            self.playr.y += y_mov
-            self.HP.y += y_mov
+        self.HP = HP
+        self.width = width
 
+    #   Передвижеие игрока и его полоски жизни
+    def pl_moving(self, x, y, avanpost):
+        self.x = x
+        self.y = y
+        if 0 < x + self.playr.x < 721 - self.width and avanpost:
+            self.playr.x += x
+            self.HP.x += x
+        if 0 < y + self.playr.y < 721 - self.width and avanpost:
+            self.playr.y += y
+            self.HP.y += y
